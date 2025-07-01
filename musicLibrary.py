2155ced1fe085072d02ca5e621a6424b11eb8259
Trivikram <trivikramkamath11@gmail.com>
2025-07-01 23:15:08 +0530

    Added music lists

---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    
+    "garuda gamana": "https://youtu.be/UtLpAuhHzNc?si=fnqmOz3lzYfILvpm",
+    "chandrachooda": "https://youtu.be/y0uZABib60g?si=3z77YN9iitdKKyH0",
+    "perfect": 	"https://www.youtube.com/watch?v=iKzRIweSBLA",
+    "shape of you":"https://youtu.be/JGwWNGJdvx8?si=4JT_GM5sp6jjqv05"
+ }
